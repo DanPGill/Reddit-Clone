847379041e0f347938cfbdda752a4cdae218d213
Dan <danpgill@gmail.com>
2017-05-02 08:55:37 +0000

    moved header.js to appropriate folder

--- header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default (props) => {
-    return (
-    <div>
-    <a href="/"><img src='https://1.bp.blogspot.com/-x55n7rdC94E/V08ovBpiFvI/AAAAAAAAAQE/b11HOh-QMtMbmsCMcFbe7AlT0ZWsY3J6ACLcB/s200/Reddit_Logo_Wide.jpg'></img></a>
-    </div>
-    )
-}
---
+++ src/components/redditClone/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <a href="/"><img src="https://1.bp.blogspot.com/-x55n7rdC94E/V08ovBpiFvI/AAAAAAAAAQE/b11HOh-QMtMbmsCMcFbe7AlT0ZWsY3J6ACLcB/s200/Reddit_Logo_Wide.jpg" /></a>
+    </div>
+  )
+}
