39eb7b463c64d2fceb788198f46ebe3aea4800a0
Dan <danpgill@gmail.com>
2017-05-03 11:59:17 +0000

    Removed unnecassary thunk, fixed reducer for request, added alert for errors

--- src/redux/actions/posts.js
+++ src/redux/actions/posts.js
@@ -1,27 +1,27 @@
 import { database } from 'firebase'
 
-export const getPostsRequested = () => dispatch => {
-  dispatch({ type: 'GET_POSTS_REQUESTED' })
+export const getPostsRequested = () => {
+  return { type: 'GET_POSTS_REQUESTED' }
 }
 
-export const getPostsRejected = () => dispatch => {
-  dispatch({ type: 'GET_POSTS_REJECTED', payload: 'An error occurred when retrieving data' })
+export const getPostsRejected = () => {
+  return { type: 'GET_POSTS_REJECTED', payload: 'An error occurred when retrieving data' }
 }
 
-export const getPostsSuccess = post => dispatch => {
-  dispatch({ type: 'GET_POSTS_SUCCESS', payload: post })
+export const getPostsSuccess = post => {
+  return { type: 'GET_POSTS_SUCCESS', payload: post }
 }
 
 export const getPosts = () => dispatch => {
   dispatch(getPostsRequested())
-  return database()
-    .ref('/posts/')
-    .once('value', snap => {
-      const post = snap.val()
-      dispatch(getPostsSuccess(post))
-    })
-    .catch(error => {
+  return database().ref('/posts/').once(
+    'value',
+    snap => {
+      dispatch(getPostsSuccess(snap.val()))
+    },
+    error => {
       console.log(error)
       dispatch(getPostsRejected())
-    })
+    },
+  )
 }
