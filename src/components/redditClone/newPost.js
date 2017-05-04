708835a907e1c4e1fac60a3af7c0e6860365e46b
Dan <danpgill@gmail.com>
2017-05-04 13:41:10 +0000

    Added labels to newPost

--- src/components/redditClone/newPost.js
+++ src/components/redditClone/newPost.js
@@ -6,11 +6,15 @@ export default props => {
     <div>
       <div>
         <Form onSubmit={() => props.onAddPost()}>
-          Link:
-          <input onChange={e => props.onUpdatePostLink(e.target.value)} value={props.currentlyEditingPost} />
+          <label htmlFor="link">
+            Link:
+          </label>
+          <input type="link" onChange={e => props.onUpdatePostLink(e.target.value)} value={props.currentlyEditingPost} />
           <br />
-          Title:
-          <input onChange={e => props.onUpdatePostTitle(e.target.value)} value={props.currentlyEditingPost} />
+          <label htmlFor="title">
+            Title:
+          </label>
+          <input type="title" onChange={e => props.onUpdatePostTitle(e.target.value)} value={props.currentlyEditingPost} />
           <br />
           <button>Create a New Post</button>
         </Form>
